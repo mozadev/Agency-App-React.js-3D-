ff46f877624cf6f39ec06523adffc119afe23777
ceosmore <ceosmore@gmail.com>
2024-04-02 13:57:29 -0500

    Agregadas dependencias de autoprefixer y postcss, y se modificó el componente Navbar.

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -7,6 +7,8 @@ import { logo, menu, close } from '../assets';
 
 const Navbar = () => {
   const [active, SetActive] = useState("");
+  const [toggle, Settoggle] = useState(false);
+
   return (
     <nav
       className={`${styles.paddingX} w-full flex
@@ -24,10 +26,69 @@ const Navbar = () => {
         >
           <img src={logo} alt='logo' className='w-9
           h-9 object-contain' />
+          <p className='text-green text-[18px]
+          font-bold cursor-pointer flex'> Cesar <span
+              className='sm:block hidden'> | Backend
+              Developer
+            </span>
+          </p>
         </Link>
+        <ul className='list-none hidden sm:flex
+        flex-row gap-10'>
+          {navLinks.map((link) => (
+            <li
+              key={link.id}
+              className={`${active === link.title
+                ? "text-white"
+                : "text-secondary"
+                } hover:text-green-400 text-[18px]`}
+              onClick={() => SetActive(link.title)}
+
+            >
+              <a href={`#${link.id}`}> {link.title} </a>
+            </li>
+          ))}
+        </ul>
+        <div className='sm:hidden flex flex-1
+        justify-end items-center'>
+          <img
+            src={toggle ? close : menu}
+            alt='menu'
+            className='w-[28px] h-[28px]
+            object-container cursor-pointer'
+            onClick={() => Settoggle(!toggle)}
+          />
+
+          <div className={`${!toggle ? "hidden"
+            : "flex"} p-6 black-gradient absolute
+           top-20 right-0 mx-4 my-2 min-w-[140px]
+           z-10 rounded-xl`}>
+
+            <ul className='list-none flex 
+            justify-end items-start flex-col gap-4'>
+              {navLinks.map((link) => (
+                <li
+                  key={link.id}
+                  className={`${active === link.title
+                    ? "text-white"
+                    : "text-secondary"
+                    } font-poppins font-medium
+                    cursor-pointer text-[16px]`}
+                  onClick={() => SetActive(link.title)}
+
+                >
+                  <a href={`#${link.id}`}> {link.title} </a>
+                </li>
+              ))}
+            </ul>
+
+
+          </div>
+
+        </div>
       </div>
     </nav>
   )
 }
 
-export default Navbar
+export default Navbar;
