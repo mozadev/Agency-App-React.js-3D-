48d01af70babf0574457809c0d6677df04a81dcf
ceosmore <ceosmore@gmail.com>
2024-04-25 18:31:23 -0500

    Update experience section

--- src/constants/index.js
+++ src/constants/index.js
@@ -129,7 +129,7 @@ const experiences = [
         iconBg: "#383E56",
         date: "March 2020 - March 2023",
         points: [
-            "Developing and maintaining web applications using React.js and other related technologies.",
+            "Developing and maintaining web applications using React.js, MongoDB, FastAPI and other related technologies.",
             "Collaborating with cross-functional teams including designers, product managers, and other developers to create high-quality products.",
             "Implementing responsive design and ensuring cross-browser compatibility.",
             "Participating in code reviews and providing constructive feedback to other developers.",
@@ -155,7 +155,7 @@ const experiences = [
         iconBg: "#383E56",
         date: "March 2023 - Present",
         points: [
-            "Developing and maintaining web applications using React.js and other related technologies.",
+            "Developing and maintaining web applications using React.js, Django, PostgreSQL and other related technologies.",
             "Collaborating with cross-functional teams including designers, product managers, and other developers to create high-quality products.",
             "Implementing responsive design and ensuring cross-browser compatibility.",
             "Participating in code reviews and providing constructive feedback to other developers.",
